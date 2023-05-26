e0899b732905d915f201043ad907639e547824f9
byungjoo0806 <andybyungjoopark@gmail.com>
2023-05-26 10:08:41 +0900

    0526 edit

--- app.js
+++ app.js
@@ -10,13 +10,25 @@
 // npm i express express-session cors
 
 const express = require("express");
-const path = require("path");
 const PORT = 5026;
+const cors = require("cors");
 const session = require("express-session");
 
 const app = express();
 
 app.use(express.urlencoded({extended : false}));
+
+// 다른 도메인에서 악의적으로 접근할 수 없도록
+// 도메인 접근시 발생하는 보안 정책
+// 다른 도메인과 통신을 안전하게 유지 시키기 위해서 보안 정책이 있다.
+// cors 모듈을 가지고 도메인을 허용 해주자
+// Access-control-Allow-origin 을 헤더에 포함해서 접근을 허용하고 응답하고
+// 브라우저에 응답을 받은 뒤, 헤더값을 확인해서 접근을 허용 또는 차단한다.
+
+//test
+app.get("/",(req,res)=>{
+    res.send("here's server's response");
+});
 
 app.listen(PORT,()=>{
     console.log("server open");
