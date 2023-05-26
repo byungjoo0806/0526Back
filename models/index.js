4f93556ca4a42d0bffe0d704f7f20123031dbb6c
byungjoo0806 <andybyungjoopark@gmail.com>
2023-05-26 10:54:38 +0900

    0526 edit

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const config = require("../config"); // 폴더까지의 경로 -> 그 폴더 내의 index.js 파일에서 내보낸 값들을 가져온다.
+const User = require("./users");
 
 const sequelize = new Sequelize(
     config.dev.database,
@@ -10,5 +11,7 @@ const sequelize = new Sequelize(
 
 const db = {};
 db.sequelize = sequelize;
+db.User = User;
+User.init(sequelize);
 
 module.exports = db;
---
+++ models/users.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+
+class User extends Sequelize.Model {
+    static init(seq){
+        return super.init({
+            // column settings
+            name : {
+                type : Sequelize.STRING(20),
+                allowNull : false
+            },
+            age : {
+                type : Sequelize.INTEGER,
+                allowNull : false
+            },
+            username : {
+                type : Sequelize.STRING(20),
+                allowNull : true
+            },
+            password : {
+                type : Sequelize.STRING(64),
+                allowNull : true
+            }
+        },{
+            // table settings
+            sequelize : seq,
+            modelName : "User", // 노드 프로젝트에서 사용할 이름 (조회 했을때 보임)
+            tableName : "users", // 데이터베이스 내에 들어갈 테이블의 이름
+            charset : "utf8", // 인코딩 방식
+            collate : "utf8_general_ci", // 인코딩 방식
+            timestamps : true,
+            paranoid : false, // 삭제시간 column 자동 생성 유무
+            underscored : false // underscored - do you want to use Camel case?
+        })
+    }
+};
+
+module.exports = User;
